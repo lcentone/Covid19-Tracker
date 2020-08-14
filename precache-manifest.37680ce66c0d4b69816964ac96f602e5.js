3a75b4c9dc099b10f4ed1d80a30357d851832c1e
Lorenzo <lorenzo@centini.info>
2020-08-14 22:16:43 +0200

    Updates

---
+++ precache-manifest.37680ce66c0d4b69816964ac96f602e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6110b87339ee6aaff756fa7babdf6de4",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "cdd781f31da982a1c5c0",
+    "url": "/Covid19-Tracker/static/css/main.ae395570.chunk.css"
+  },
+  {
+    "revision": "d1fbf42c8f635656fa5c",
+    "url": "/Covid19-Tracker/static/js/2.e88aec46.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid19-Tracker/static/js/2.e88aec46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd781f31da982a1c5c0",
+    "url": "/Covid19-Tracker/static/js/main.e85eee57.chunk.js"
+  },
+  {
+    "revision": "ac317361a16e299da802",
+    "url": "/Covid19-Tracker/static/js/runtime-main.bcd7ed5b.js"
+  }
+]);
